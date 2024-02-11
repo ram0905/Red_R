347dced8c3ed06a8eefffeb3412863b90412e4d1
R.O. <ramos16qa@gmail.com>
2024-02-10 19:04:56 -0700

    commit_3j

--- src/HW/HW_6_2.java
+++ src/HW/HW_6_2.java
@@ -2,8 +2,7 @@ package HW;
 
 public class HW_6_2 {
     public static void main(String[] args) {
-
-         System.out.println("Hello World");
+        System.out.println("Hello World");
 
 
     }
